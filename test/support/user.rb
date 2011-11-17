a0b7be3fa7dd479980714c3a39fce0be280aab84
Kelly <kelly@kelredd.com>
2011-11-17 11:56:39 -0600

    allow for just including on NsOptions
    * NsOptions, on included, includes HasOptions for you
    * I just changed the App support module to include this way and all tests passed so I figure this is good

--- test/support/user.rb
+++ test/support/user.rb
@@ -1,5 +1,8 @@
 class User
+
+  # mixin using the specific HasOptions variant
   include NsOptions::HasOptions
+
   options(:preferences, 'user-preferences') do
     option :home_url
     option :show_messages,  NsOptions::Option::Boolean, :require => true
