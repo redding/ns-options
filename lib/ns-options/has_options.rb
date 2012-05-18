9132fec83532f8e2893966102827e57d4cd0cd1e
Kelly <kelly@kellyredding.com>
2012-05-18 09:07:03 -0500

    cleaned up the comments on `has_options.rb`
    * respect 80 char width
    * fixed a typo

--- lib/ns-options/has_options.rb
+++ lib/ns-options/has_options.rb
@@ -13,11 +13,13 @@ module NsOptions
 
     module DSL
 
-      # This is the main DSL method for creating a namespace of options for your class/module. This
-      # will define a class method for both classes and modules and an additional instance method
-      # for classes. The namespace is then created and returned by calling the class method version.
-      # For classes, the instance method will build an entirely new namespace from the class level
-      # namespace. This is so when you define options at the class level:
+      # This is the main DSL method for creating a namespace of options for your
+      # class/module. This will define a class method for both classes and
+      # modules and an additional instance method for classes. The namespace is
+      # then created and returned by calling the class method version.  For
+      # classes, the instance method will build an entirely new namespace from
+      # the class level namespace. This is so when you define options at the
+      # class level:
       #
       # class Something
       #   include NsOptions
@@ -26,16 +28,16 @@ module NsOptions
       #   end
       # end
       #
-      # the namespaces at the instance level still get all the defined options, but are completely
-      # separate objects from the class and other instances. Modules only deal with a single
-      # namespace at the module level.
+      # the namespaces at the instance level still get all the defined options,
+      # but are completely separate objects from the class and other instances.
+      # Modules only deal with a single namespace at the module level.
 
       # The options method takes three args:
       # * `name` : what to name the defined methods for accessing the namespace
       # * `key`  : (optional) what to key the created namespace objects with
       #            - defaults to `name`
       #            - useful if persisting namespaces into some key-value store
-      # * `block`: (optional) a predefined set up nested options and namespaces
+      # * `block`: (optional) a predefined set of nested options and namespaces
 
       def options(name, key = nil, &block)
         NsOptions::Helper.advisor.is_this_namespace_ok?(name, caller)
